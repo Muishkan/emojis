9ff6a956929afede78c546821e9a2c45a15f36ef
Masood AHMAD <mas@outlook.fr>
2021-11-05 09:11:43 +0100

    Removed ;

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="container">
       <Search str={str} setStr={setStr} setResults={setResults} />
-      {searchResult()};
+      {searchResult()}
       <Footer />
     </div>
   );
